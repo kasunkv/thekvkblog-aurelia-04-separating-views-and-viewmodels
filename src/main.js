fc3997d8c56fb83ca3f07d4df32b3704f1253fc6
kasunkv <kvkrusl@gmail.com>
2016-09-19 21:18:42 +0530

    Finished demo

--- src/main.js
+++ src/main.js
@@ -1,10 +1,18 @@
 import {LogManager} from 'aurelia-framework';
 import {ConsoleAppender} from 'aurelia-logging-console';
+import {ViewLocator} from 'aurelia-framework';
 
 LogManager.addAppender(new ConsoleAppender());
 LogManager.setLevel(LogManager.logLevel.debug);
 
 export function configure(aurelia) {
+
+    ViewLocator.prototype.convertOriginToViewUrl = (origin) => {
+        let moduleId = origin.moduleId;
+        let id = moduleId.endsWith('.js') ? moduleId.substring(0, moduleId.length - 3) : moduleId;
+        return `${id.replace('viewmodels', 'views')}.html`;
+    };
+
     aurelia.use.standardConfiguration();
-    aurelia.start().then(a => a.setRoot('shell', document.getElementById('app-container')));
+    aurelia.start().then(a => a.setRoot('viewmodels/shell', document.getElementById('app-container')));
 }
